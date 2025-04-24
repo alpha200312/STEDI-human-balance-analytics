689a9a78fa0fc9f11a552c46d4c403a49ee7d916
omkar gadhave <106884114+alpha200312@users.noreply.github.com>
2025-04-24 20:35:44 +0530

    Update Customer_landing_to_trusted .py

--- job scripts (python files)/Customer_landing_to_trusted .py
+++ job scripts (python files)/Customer_landing_to_trusted .py
@@ -5,13 +5,8 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsgluedq.transforms import EvaluateDataQuality
-from awsglue import DynamicFrame
+import re
 
-def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
-    for alias, frame in mapping.items():
-        frame.toDF().createOrReplaceTempView(alias)
-    result = spark.sql(query)
-    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -26,20 +21,16 @@ DEFAULT_DATA_QUALITY_RULESET = """
     ]
 """
 
-# Script generated for node Customer Landing
-CustomerLanding_node1745218107609 = glueContext.create_dynamic_frame.from_catalog(database="glue-databe", table_name="glue_table", transformation_ctx="CustomerLanding_node1745218107609")
-
-# Script generated for node SQL Query
-SqlQuery114 = '''
-select * from myDataSource
-
-'''
-SQLQuery_node1745218131512 = sparkSqlQuery(glueContext, query = SqlQuery114, mapping = {"myDataSource":CustomerLanding_node1745218107609}, transformation_ctx = "SQLQuery_node1745218131512")
-
 # Script generated for node Amazon S3
-EvaluateDataQuality().process_rows(frame=SQLQuery_node1745218131512, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1745218098947", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
-AmazonS3_node1745218152104 = glueContext.getSink(path="s3://udacity-lake1", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1745218152104")
-AmazonS3_node1745218152104.setCatalogInfo(catalogDatabase="glue-databe",catalogTableName="glue_table")
-AmazonS3_node1745218152104.setFormat("json")
-AmazonS3_node1745218152104.writeFrame(SQLQuery_node1745218131512)
+AmazonS3_node1745260010351 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://udacity-lake1/customer/landing/"], "recurse": True}, transformation_ctx="AmazonS3_node1745260010351")
+
+# Script generated for node PrivacyFilter
+PrivacyFilter_node1745260782345 = Filter.apply(frame=AmazonS3_node1745260010351, f=lambda row: (not(row["shareWithResearchAsOfDate"] == 0)), transformation_ctx="PrivacyFilter_node1745260782345")
+
+# Script generated for node Trusted Customer Zone
+EvaluateDataQuality().process_rows(frame=PrivacyFilter_node1745260782345, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1745260000407", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+TrustedCustomerZone_node1745260793967 = glueContext.getSink(path="s3://udacity-lake1/customer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="TrustedCustomerZone_node1745260793967")
+TrustedCustomerZone_node1745260793967.setCatalogInfo(catalogDatabase="stedi",catalogTableName="customer_trusted")
+TrustedCustomerZone_node1745260793967.setFormat("json")
+TrustedCustomerZone_node1745260793967.writeFrame(PrivacyFilter_node1745260782345)
 job.commit()
